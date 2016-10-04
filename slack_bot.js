2f28b6855239f2625cbb330bfc3271f84f84700d
dmathew93 <denzel.mathew@gmail.com>
2016-10-04 13:16:26 -0400

    PLEASE WORK

---
+++ slack_bot.js
@@ -0,0 +1,15 @@
+var Botkit = require('./node_modules/botkit/lib/Botkit.js');
+var controller = Botkit.slackbot({
+  debug: false
+});
+
+controller.spawn({
+  token: 'xoxb-77604885776-8WlYCOYhUOFF3m8XAQTunNAH'
+}).startRTM()
+
+// give the bot something to listen for.
+controller.hears('hello',['direct_message','direct_mention','mention'],function(bot,message) {
+
+  bot.reply(message,'Hello yourself.');
+
+});
