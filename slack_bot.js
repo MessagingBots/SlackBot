6c9128342c1b879c32cc175310a99b22d70d1dda
dmathew93 <denzel.mathew@gmail.com>
2016-10-04 15:09:34 -0400

    FUC

--- slack_bot.js
+++ slack_bot.js
@@ -10,6 +10,6 @@ controller.spawn({
 // give the bot something to listen for.
 controller.hears('hello',['direct_message','direct_mention','mention'],function(bot,message) {
 
-  bot.reply(message,'Hello yourself.');
+  bot.reply(message,'Hello ydfdfourself.');
 
 });
