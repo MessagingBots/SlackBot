a887132281a437676864657eaf3fe64de6b5f460
dmathew93 <denzel.mathew@gmail.com>
2016-10-16 17:28:30 -0400

    playing around with botkit to understand more

--- slack_bot.js
+++ slack_bot.js
@@ -1,15 +1,23 @@
+//Botkit loaded in
 var Botkit = require('./node_modules/botkit/lib/Botkit.js');
+
+//the bot controller
 var controller = Botkit.slackbot({
   debug: false
 });
 
-controller.spawn({
+//spawn a bot based on the API Token
+var bot = controller.spawn({
   token: 'xoxb-77604885776-8WlYCOYhUOFF3m8XAQTunNAH'
 }).startRTM()
 
-// give the bot something to listen for.
-controller.hears('hello',['direct_message','direct_mention','mention'],function(bot,message) {
 
-  bot.reply(message,'Hello ydfdfourself.');
+//Test Case, listen for pizza orders
+controller.hears('pizza',['direct_mention','direct_message','mention'],function(bot,message){
+  bot.reply(message,'What kind of pizza?');
+});
 
+//testing if message received?
+controller.on('message_received',function(bot,message){
+  bot.reply("I received a message.");
 });
