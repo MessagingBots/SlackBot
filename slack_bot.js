b7ab5f1495e42f62f3cf38dcb25e6aa99d49844e
dmathew93 <denzel.mathew@gmail.com>
2016-10-25 01:15:07 -0400

    testing out heroku deployment with worker instead of web

--- slack_bot.js
+++ slack_bot.js
@@ -17,7 +17,21 @@ controller.hears('pizza',['direct_mention','direct_message','mention'],function(
   bot.reply(message,'What kind of pizza?');
 });
 
-//testing if message received?
-controller.on('message_received',function(bot,message){
+
+
+/// ----- MESSAGE HANDLERS ----- ////
+//So these are message event handlers; the slack stream can send the bot anything and
+controller.on('direct_message',function(bot,message){
   bot.reply(message,"I received a message.");
 });
+
+
+
+/*
+
+The slack bot should be able to do the following
+  1. when a user requests a pizza:
+  2. query for toppings
+  3. when receive done
+  4. launch up dominos.com
+*/
